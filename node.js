7ba8f32394cfd5a13203a53426952b7a4f500c12
daithang28 <66111306+daithang28@users.noreply.github.com>
2020-06-12 12:05:07 +0700

    Update node.js

--- node.js
+++ node.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var lanthu = 0;
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 function XuLyWEB(req, res) {
     lanthu++;
     res.writeHead(200, {'Content-Type': 'text/html'});
@@ -11,5 +11,5 @@ function XuLyWEB(req, res) {
     );
     console.log("\n\t ...lan thu - " + lanthu);
 }
-http.createServer(XuLyWEB).listen(port);
+http.createServer(XuLyWEB).listen(PORT);
 console.log("Nodejs dang RUN o Server")
